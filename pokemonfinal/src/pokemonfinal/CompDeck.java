560b16e6a6907394f20673e46d3a7dc9c28041c0
Claire.Krawiec <Claire.Krawiec@MU-C1-02.mullen.pvt.k12.co.us>
2016-02-05 13:45:59 -0700

    kydg

--- pokemonfinal/src/pokemonfinal/CompDeck.java
+++ pokemonfinal/src/pokemonfinal/CompDeck.java
@@ -8,27 +8,27 @@ public class CompDeck
 		public static void fillCompDeck()
 		{
 			compDeck.add(new PokemonCard(10, "Zigzagoon", "double stab"));
-			compDeck.add(new PokemonCard(10, "taillow", "skill dive"));
-			compDeck.add(new PokemonCard(10, "barboach", "waterfall"));
-			compDeck.add(new PokemonCard(30, "porygon", "sharpen"));
-			compDeck.add(new PokemonCard(30, "electrode", "reckless bomb"));
-			compDeck.add(new PokemonCard(10, "zubat", "poison fang"));
-			compDeck.add(new PokemonCard(20, "corsola", "double attack"));
+			compDeck.add(new PokemonCard(10, "Taillow", "skill dive"));
+			compDeck.add(new PokemonCard(10, "Barboach", "waterfall"));
+			compDeck.add(new PokemonCard(30, "Porygon", "sharpen"));
+			compDeck.add(new PokemonCard(30, "Electrode", "reckless bomb"));
+			compDeck.add(new PokemonCard(10, "Zubat", "poison fang"));
+			compDeck.add(new PokemonCard(20, "Corsola", "double attack"));
 			compDeck.add(new PokemonCard(20, "Roselia", "flick poison"));
-			compDeck.add(new PokemonCard(10, "beldum", "magnetic lines"));
+			compDeck.add(new PokemonCard(10, "Beldum", "magnetic lines"));
 			compDeck.add(new PokemonCard(30, "Combusken", "fire breathing"));
 			compDeck.add(new PokemonCard(60, "Magikarp", "dragon rage"));
 			compDeck.add(new PokemonCard(30, "Whismur", "supersonic"));
-			compDeck.add(new PokemonCard(10, "meditite", "kick"));
-			compDeck.add(new PokemonCard(10, "electrike", "quick blow"));
+			compDeck.add(new PokemonCard(10, "Meditite", "kick"));
+			compDeck.add(new PokemonCard(10, "Electrike", "quick blow"));
 			compDeck.add(new PokemonCard(20, "Diglett", "sand pitt"));
-			compDeck.add(new PokemonCard(20, "nidoran", "poison sting"));
+			compDeck.add(new PokemonCard(20, "Nidoran", "poison sting"));
 			compDeck.add(new PokemonCard(20, "Treecko", "paralyzing gaze"));
 			compDeck.add(new PokemonCard(20, "Staryu", "Spinning attack"));
 			compDeck.add(new PokemonCard(10, "Marill", "rollout"));
 			compDeck.add(new PokemonCard(20, "Drowzee", "headbutt"));
 			compDeck.add(new PokemonCard(10, "Phanpy", "mud slam"));
-			compDeck.add(new PokemonCard(30, "machop", "submission"));
+			compDeck.add(new PokemonCard(30, "Machop", "submission"));
 			compDeck.add(new PokemonCard(100, "Pikachu", "thunder wave"));
 			compDeck.add(new PokemonCard(30, "Corphish", "corkscrew punch"));
 			compDeck.add(new PokemonCard(10, "Mankey", "Rage"));
--- pokemonfinal/src/pokemonfinal/PokemonRunner1.java
+++ pokemonfinal/src/pokemonfinal/PokemonRunner1.java
@@ -1,65 +1,179 @@
 package pokemonfinal;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class PokemonRunner1
-	{
-		public static int playerCards;
-		public static int compCards;
-		public static int drawCard;
-		public static int askQuestion;
-		 public static ArrayList <UserDeck> userHand = new ArrayList <UserDeck>();
+    {
+        public static int playerCards;
+        public static int compCards;
+        public static int drawCard;
+        public static int askQuestion;
+        public static int menuChoice = 1;
+        public static int playAgain = 1;
+        public static boolean play = true;
+         public static ArrayList <UserDeck> userHand = new ArrayList <UserDeck>();
          public static ArrayList <CompDeck> computerHand = new ArrayList <CompDeck>();
-		public static void main(String[] args)
-			{
-				UserDeck.fillUserDeck();
-				CompDeck.fillCompDeck();
-				UserDeck.shuffleUserDeck();
-				CompDeck.shuffleCompDeck();
-				
-					for(int i = 0; i > 3; i++)
-						{
-							  UserDeck.userDeck.get(0);
-							  UserDeck.userDeck.get(1);
-							  UserDeck.userDeck.get(2);
-							  UserDeck.userDeck.get(3);
-							
-						}
-						
-						System.out.println("Welcome to Pokemon! your first card is " + UserDeck.userDeck.get(0).getName());
-						System.out.println();
-						System.out.println("Your second card is " + UserDeck.userDeck.get(1).getName());
-						System.out.println();
-						System.out.println("Your third card is " + UserDeck.userDeck.get(2).getName());
-						System.out.println();
-						System.out.println("your fourth card is " + UserDeck.userDeck.get(3).getName());
-						System.out.println("What card would you like to play?");
-						   Scanner userInput = new Scanner (System.in);
-                           userInput.nextLine();
-				
+        public static void main(String[] args)
+            {
+            	while(play)
+            		{
+            			
+            		
+                UserDeck.fillUserDeck();
+                CompDeck.fillCompDeck();
+                UserDeck.shuffleUserDeck();
+                CompDeck.shuffleCompDeck();
+                
+                    for(int i = 0; i > 3; i++)
+                        {
+                              UserDeck.userDeck.get(0);
+                              UserDeck.userDeck.get(1);
+                              UserDeck.userDeck.get(2);
+                              UserDeck.userDeck.get(3);
+                            
+                        }
+                    for(int i = 0; i > 3; i++)
+                    {
+                        CompDeck.compDeck.get(0);
+                        CompDeck.compDeck.get(1);
+                        CompDeck.compDeck.get(2);
+                        CompDeck.compDeck.get(3);
+                        
+                    }
+                        
+                        System.out.println("Welcome to Pokemon! ");
+                        		System.out.println("your first card is " + UserDeck.userDeck.get(0).getName());
+                        System.out.println();
+                        System.out.println("Your second card is " + UserDeck.userDeck.get(1).getName());
+                        System.out.println();
+                        System.out.println("Your third card is " + UserDeck.userDeck.get(2).getName());
+                        System.out.println();
+                        System.out.println("your fourth card is " + UserDeck.userDeck.get(3).getName());
+                        System.out.println();
+                        System.out.println("What card would you like to play?");
+                        askQuestion();
+                        switch (menuChoice)
+                        {
+                        case 1:
+                            System.out.println("You played " + UserDeck.userDeck.get(0).getName() + ", its power was " +UserDeck.userDeck.get(0).getPower());
+                                System.out.println("The computer's card was " + CompDeck.compDeck.get(0).getName() + ", its power was " + CompDeck.compDeck.get(0).getPower());
+                                    if(UserDeck.userDeck.get(0).getPowerNum() == CompDeck.compDeck.get(0).getPowerNum())
+                                    {
+                                        System.out.println("Your pokemon's tied! crazy!");
+                                    }
+                                    if(UserDeck.userDeck.get(0).getPowerNum() > CompDeck.compDeck.get(0).getPowerNum())
+                                    {
+                                        System.out.println("Your card " + UserDeck.userDeck.get(0).getName() + " beat the computer's card " + CompDeck.compDeck.get(0).getName());
+                                        System.out.println("YOU WIN!!!!");
+                                        System.out.println();
+                                        System.out.println("Catch them all!");
+                                    }
+                                    if(UserDeck.userDeck.get(0).getPowerNum() < CompDeck.compDeck.get(0).getPowerNum())
+                                    {
+                                        System.out.println("Your card " + UserDeck.userDeck.get(0).getName() + " was beaten by the computer's card " + CompDeck.compDeck.get(0).getName());
+                                        System.out.println("You lose.");
+                                    }
+                            break;
+                        case 2:
+                            System.out.println("You played " + UserDeck.userDeck.get(1).getName() + " it power was " + UserDeck.userDeck.get(1).getPower());
+                            System.out.println("The computer's card was " + CompDeck.compDeck.get(1).getName() + ", and its power was " + CompDeck.compDeck.get(1).getPower());
+                                if(UserDeck.userDeck.get(1).getPowerNum() == CompDeck.compDeck.get(1).getPowerNum())
+                                {
+                                    System.out.println("Your pokemons tied! crazy!");
+                                }
+                                if(UserDeck.userDeck.get(1).getPowerNum() > CompDeck.compDeck.get(1).getPowerNum())
+                                {
+                                    System.out.println("Your card " + UserDeck.userDeck.get(1).getName() + " beat the computer's card " + CompDeck.compDeck.get(1).getName());
+                                    System.out.println("YOU WIN!!!!!!!");
+                                    System.out.println();
+                                    System.out.println("Catch them all!");
+                                }
+                                if(UserDeck.userDeck.get(1).getPowerNum() < CompDeck.compDeck.get(1).getPowerNum())
+                                {
+                                    System.out.println("Your card " + UserDeck.userDeck.get(1).getName() + " was beaten by the computers card " + CompDeck.compDeck.get(1).getName());
+                                    System.out.println("You lose.");
+                                }
+                            break;
+                        case 3:
+                            System.out.println("You played " + UserDeck.userDeck.get(2).getName() + ", its power was " + UserDeck.userDeck.get(2).getPower());
+                            System.out.println("The computer's card was " + CompDeck.compDeck.get(2).getName() + ", and its power was " + CompDeck.compDeck.get(2).getPower());
+                            if(UserDeck.userDeck.get(2).getPowerNum() == CompDeck.compDeck.get(2).getPowerNum())
+                            {
+                                System.out.println("Your pokemons tied! Crazy!");
+                            }
+                            if(UserDeck.userDeck.get(2).getPowerNum() > CompDeck.compDeck.get(2).getPowerNum())
+                            {
+                                System.out.println("Your card " + UserDeck.userDeck.get(2).getName() + " beat the computer's card " + CompDeck.compDeck.get(2).getName());
+                                System.out.println("YOU WIN!!!");
+                                System.out.println();
+                                System.out.println("Catch them all!");
+                            }
+                            if(UserDeck.userDeck.get(2).getPowerNum() < CompDeck.compDeck.get(2).getPowerNum())
+                            {
+                                System.out.println("Your card " + UserDeck.userDeck.get(2).getName() + " was beaten by the computer's card " + CompDeck.compDeck.get(2).getName());
+                                System.out.println("You lose.");
+                            }
+                            break;
+                        case 4:
+                            System.out.println("You played " + UserDeck.userDeck.get(3).getName() + ", its power was " + UserDeck.userDeck.get(3).getPower());
+                            System.out.println("The computer's card was " + CompDeck.compDeck.get(3).getName() + ", and its power was " + CompDeck.compDeck.get(3).getPower());
+                            if(UserDeck.userDeck.get(3).getPowerNum() == CompDeck.compDeck.get(3).getPowerNum())
+                            {
+                                System.out.println("Your pokemons tied! Crazy!");
+                            }
+                            if(UserDeck.userDeck.get(3).getPowerNum() > CompDeck.compDeck.get(3).getPowerNum())
+                            {
+                                System.out.println("Your card " + UserDeck.userDeck.get(3).getName() + " beat the computer's card " + CompDeck.compDeck.get(3).getName());
+                                System.out.println("YOU WIN!!!!!!!");
+                                System.out.println();
+                                System.out.println("Catch them all!");
+                            }
+                            if(UserDeck.userDeck.get(3).getPowerNum() < CompDeck.compDeck.get(3).getPowerNum())
+                            {
+                                System.out.println("Your card " + UserDeck.userDeck.get(3).getName() + " was beaten by the computer's card " + CompDeck.compDeck.get(3).getName());
+                                System.out.println("You lose.");
+                            }
+                            break;
+                        case 5:
+                        	System.out.println("please choose a number 1-4");
+                        	askQuestion();
+                        	break;
+                            
+                        }
+                        System.out.println("Would you like to play again?");
+                        System.out.println("1. Heck yes!");
+                        System.out.println("2. No thanks.");
+                        Scanner userInput = new Scanner (System.in);
+                    String input = userInput.nextLine();
+                        
+                       if(input.equals("1"))
+                    	   {
+                    		   
+                    	   }
+                    	   
+                            System.out.println("Thanks for playing. remember to catch them all!");
+                            play= false;
+                        }
+                        
+            		}
+                        
+                
 
-			}
-		public static void askQuestion()
-		{
-			System.out.println("1. first card");
-			System.out.println("2. second card");
-			System.out.println("3. third card");
-			System.out.println("4. fourth card");
-			if(askQuestion == 1)
-				{
-					System.out.println("You played " + UserDeck.userDeck.get(0).getName() + " and its power is " + UserDeck.userDeck.get(0).getPower());
-				}
-			if(askQuestion == 2)
-				{
-					System.out.println("You played " + UserDeck.userDeck.get(1).getName() + " and its power is " + UserDeck.userDeck.get(1).getPower());
-				}
-			if(askQuestion == 3)
-				{
-					System.out.println("You played " + UserDeck.userDeck.get(2).getName() + " and its power is " + UserDeck.userDeck.get(2).getPower());
-				}
-			if(askQuestion == 4)
-				{
-					System.out.println("you played " + UserDeck.userDeck.get(4).getName() + " and its power is " + UserDeck.userDeck.get(4).getPower());
-				}
-		}
+            }
+        public static void askQuestion()
+        {
+            
+            System.out.println("1. first card");
+            System.out.println("2. second card");
+            System.out.println("3. third card");
+            System.out.println("4. fourth card");
+            Scanner user_Input = new Scanner (System.in);
+            user_Input.nextLine();
+            
+            
+            
+        
+        }
 
-	}
+    }
+
